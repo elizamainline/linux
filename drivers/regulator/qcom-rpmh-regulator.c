f475f2925e0bb9bdd9509bee36465f187518a92d
Oleksii Onchul <oleksiionchul@gmail.com>
2026-09-22 13:17:35 +0300

    regulator: qcom-rpmh: Add PMXR2230 support
    Add the PMXR2230 RPMh VRM resource layout so boards can control its
    four SMPS, LDO1-2, LDO4-23 and BOB regulators.
    
    Signed-off-by: Oleksii Onchul <oleksiionchul@gmail.com>

--- drivers/regulator/qcom-rpmh-regulator.c
+++ drivers/regulator/qcom-rpmh-regulator.c
@@ -1843,6 +1843,37 @@ static const struct rpmh_vreg_init_data pm7550_vreg_data[] = {
 	{}
 };
 
+static const struct rpmh_vreg_init_data pmxr2230_vreg_data[] = {
+	RPMH_VREG("smps1", SMPS, 1, &pmic5_ftsmps525,  "vdd-s1"),
+	RPMH_VREG("smps2", SMPS, 2, &pmic5_ftsmps525,  "vdd-s2"),
+	RPMH_VREG("smps3", SMPS, 3, &pmic5_ftsmps525,  "vdd-s3"),
+	RPMH_VREG("smps4", SMPS, 4, &pmic5_ftsmps525,  "vdd-s4"),
+	RPMH_VREG("ldo1",  LDO,  1, &pmic5_nldo515,    "vdd-l1"),
+	RPMH_VREG("ldo2",  LDO,  2, &pmic5_nldo515,    "vdd-l2"),
+	RPMH_VREG("ldo4",  LDO,  4, &pmic5_nldo515,    "vdd-l4-l5"),
+	RPMH_VREG("ldo5",  LDO,  5, &pmic5_nldo515,    "vdd-l4-l5"),
+	RPMH_VREG("ldo6",  LDO,  6, &pmic5_nldo515,    "vdd-l6"),
+	RPMH_VREG("ldo7",  LDO,  7, &pmic5_nldo515,    "vdd-l7"),
+	RPMH_VREG("ldo8",  LDO,  8, &pmic5_nldo515,    "vdd-l8"),
+	RPMH_VREG("ldo9",  LDO,  9, &pmic5_nldo515,    "vdd-l9-l10"),
+	RPMH_VREG("ldo10", LDO, 10, &pmic5_nldo515,    "vdd-l9-l10"),
+	RPMH_VREG("ldo11", LDO, 11, &pmic5_nldo515,    "vdd-l11"),
+	RPMH_VREG("ldo12", LDO, 12, &pmic5_pldo515_mv, "vdd-l12-l14"),
+	RPMH_VREG("ldo13", LDO, 13, &pmic5_pldo515_mv, "vdd-l13-l16"),
+	RPMH_VREG("ldo14", LDO, 14, &pmic5_pldo,       "vdd-l12-l14"),
+	RPMH_VREG("ldo15", LDO, 15, &pmic5_pldo,       "vdd-l15-l17-l18-l19-l20-l21-l22-l23"),
+	RPMH_VREG("ldo16", LDO, 16, &pmic5_pldo,       "vdd-l13-l16"),
+	RPMH_VREG("ldo17", LDO, 17, &pmic5_pldo,       "vdd-l15-l17-l18-l19-l20-l21-l22-l23"),
+	RPMH_VREG("ldo18", LDO, 18, &pmic5_pldo,       "vdd-l15-l17-l18-l19-l20-l21-l22-l23"),
+	RPMH_VREG("ldo19", LDO, 19, &pmic5_pldo,       "vdd-l15-l17-l18-l19-l20-l21-l22-l23"),
+	RPMH_VREG("ldo20", LDO, 20, &pmic5_pldo,       "vdd-l15-l17-l18-l19-l20-l21-l22-l23"),
+	RPMH_VREG("ldo21", LDO, 21, &pmic5_pldo,       "vdd-l15-l17-l18-l19-l20-l21-l22-l23"),
+	RPMH_VREG("ldo22", LDO, 22, &pmic5_pldo,       "vdd-l15-l17-l18-l19-l20-l21-l22-l23"),
+	RPMH_VREG("ldo23", LDO, 23, &pmic5_pldo,       "vdd-l15-l17-l18-l19-l20-l21-l22-l23"),
+	RPMH_VREG("bob",   BOB,  1, &pmic5_bob,        "vdd-bob"),
+	{}
+};
+
 static const struct rpmh_vreg_init_data pmr735a_vreg_data[] = {
 	RPMH_VREG("smps1", SMPS, 1, &pmic5_ftsmps520, "vdd-s1"),
 	RPMH_VREG("smps2", SMPS, 2, &pmic5_ftsmps520, "vdd-s2"),
@@ -2096,6 +2127,10 @@ static const struct of_device_id __maybe_unused rpmh_regulator_match_table[] = {
 	{
 		.compatible = "qcom,pmx75-rpmh-regulators",
 		.data = pmx75_vreg_data,
+	},
+	{
+		.compatible = "qcom,pmxr2230-rpmh-regulators",
+		.data = pmxr2230_vreg_data,
 	},
 	{
 		.compatible = "qcom,pm7325-rpmh-regulators",
